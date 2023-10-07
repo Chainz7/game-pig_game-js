ba4078f4001f0c34c4dbab11fe39847c6ad8e6c3
Chainz <61576690+Chainz7@users.noreply.github.com>
2023-10-07 14:25:35 +0700

    Resetting the game

--- script.js
+++ script.js
@@ -12,17 +12,30 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+// Declaring variable
+let scores, currentScore, activePlayer, playing;
+
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
-// Reusable code
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+
+init();
+
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -32,7 +45,7 @@ const switchPlayer = function () {
 };
 
 // Resetting game functionality
-btnNew.addEventListener("click", function () {});
+btnNew.addEventListener("click", init);
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
